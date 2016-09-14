6bc88165e5fd85a8d67ae34eb0f7fbc97b6b3028
manikumarp <manikumarp439@gmail.com>
2016-09-14 14:30:04 +0530

    SecondViewController is added
    Worked on Story Board

---
+++ demogit/demogit/SecondViewController.h
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.h
+//  demogit
+//
+//  Created by user on 14/9/16.
+//  Copyright © 2016 kishore kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+{
+    
+ IBOutlet UIImageView *ImageView;
+}
+
+@end
---
+++ demogit/demogit/SecondViewController.m
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.m
+//  demogit
+//
+//  Created by user on 14/9/16.
+//  Copyright © 2016 kishore kumar. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@implementation SecondViewController
+
+@end
