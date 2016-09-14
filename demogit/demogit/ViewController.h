163ac9cf78e1177dc9f222f8f1fff494c6e4e7c6
kishoremobello <kishore@mobellotech.com>
2016-09-14 12:18:53 +0530

    Initialised github demo project to learn about Xcode git extension

---
+++ demogit/demogit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demogit
+//
+//  Created by kishore kumar on 14/09/16.
+//  Copyright © 2016 kishore kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ demogit/demogit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  demogit
+//
+//  Created by kishore kumar on 14/09/16.
+//  Copyright © 2016 kishore kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
