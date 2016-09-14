b1ab9f3d8de01f034f2629fb8f77bcdd3feb815f
kishoremobello <kishore@mobellotech.com>
2016-09-14 13:20:10 +0530

    added one more viewController
    Added onemore view controller for verification

---
+++ demogit/demogit/FirstViewController/FirstViewController.h
@@ -0,0 +1,14 @@
+//
+//  FirstViewController.h
+//  demogit
+//
+//  Created by kishore kumar on 14/09/16.
+//  Copyright © 2016 kishore kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *lblfirstviewcontroller;
+
+@end
